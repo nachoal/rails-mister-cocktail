82c52bc0466a80bb65388b1028a0fad165b6e604
Ignacio Alonso <nacho.allibre@gmail.com>
2018-10-26 11:33:27 -0500

    add more styles, change fonts

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,8 @@
 class Dose < ApplicationRecord
+  # validates :cocktail, presence: true, uniqueness: true
+  # validates :ingredient, presence: true, uniqueness: true
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  # validates :cocktail, presence: true, uniqueness: true
-  # validates :ingredient, presence: true, uniqueness: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
